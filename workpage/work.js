bf39ad8b93d08ca29ed4d21b904656cf69e6941d
rui <82731268+liaosirui333-s@users.noreply.github.com>
2022-12-22 18:22:04 -0500

    89%

--- workpage/work.js
+++ workpage/work.js
@@ -8,12 +8,37 @@ images.forEach(image => {
         lightbox.classList.add('active')
         const img = document.createElement('img')
         img.src = image.src
+
         while (lightbox.firstChild) {
             lightbox.removeChild(lightbox.firstChild)
         }
         lightbox.appendChild(img)
     })
 })
+
+const videos = document.querySelectorAll('video')
+videos.forEach(video => {
+    video.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const vid = document.createElement('video')
+        vid.src = video.src
+
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(vid)
+    })
+    video.addEventListener("mouseover", e => {
+        video.play();
+    })
+    video.addEventListener("mouseout", e => {
+        video.pause();
+    })
+})
+
+
+
+
 
 lightbox.addEventListener('click', e => {
     if (e.target !== e.currentTarget) return
