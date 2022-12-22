80fabb366aea7c8b801a3a02d989afa16443067a
rui <82731268+liaosirui333-s@users.noreply.github.com>
2022-12-22 00:36:33 -0500

    style 80% done

--- workpage/work.js
+++ workpage/work.js
@@ -1,19 +1,21 @@
-let slideIndex = [1, 1];
-let slideId = ["project1"]
-showSlides(1, 0);
-showSlides(1, 1);
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
 
-function plusSlides(n, no) {
-    showSlides(slideIndex[no] += n, no);
-}
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.src
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+    })
+})
 
-function showSlides(n, no) {
-    let i;
-    let x = document.getElementsByClassName(slideId[no]);
-    if (n > x.length) { slideIndex[no] = 1 }
-    if (n < 1) { slideIndex[no] = x.length }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex[no] - 1].style.display = "block";
-}
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
